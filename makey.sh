47a9da6b9aa4bdd8c192e83c9dbb0c761c22ffbc
Matthew Chen <charlesmchen@gmail.com>
2014-04-14 00:54:12 -0400

    Update pages.

--- makey.sh
+++ makey.sh
@@ -4,5 +4,6 @@ curl https://raw.github.com/charlesmchen/WeView2/master/CHANGELOG.md >> CHANGELO
 echo "<!-- TEMPLATE END -->" >> CHANGELOG.md
 python ./config_md.py 
 jekyll build
+cp _site/*.html .
 cp _site/home.html _site/index.html
 cp _site/home.html index.html
