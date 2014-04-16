e2d342ca852edd5176665ae8c887c1160a577afe
Matthew Chen <charlesmchen@gmail.com>
2014-04-16 09:36:02 -0400

    Update pages.

--- makey.sh
+++ makey.sh
@@ -1,10 +1,19 @@
-rm *.html
+#!/bin/sh
+
+# Fail if we reference an undefined variable.
+set -o nounset
+# Fail if any command fails (doesn't include piped commands).
+set -o errexit
+# Fail if any piped command fails.
+set -o pipefail
+
+# rm *.html
 rm CHANGELOG.md
 echo "<!-- TEMPLATE START -->" >> CHANGELOG.md
 curl https://raw.github.com/charlesmchen/WeView2/master/CHANGELOG.md >> CHANGELOG.md
 echo "<!-- TEMPLATE END -->" >> CHANGELOG.md
-python ./config_md.py 
+python ./config_md.py
 jekyll build
-cp _site/*.html .
+# cp _site/*.html .
 cp _site/home.html _site/index.html
 cp _site/home.html index.html
